5697c6e8d918656394d63b0fdef4fd6826b3f67b
Bryce Davie <daviebry@shernet.sheridancollege.ca>
2024-02-07 16:13:48 -0500

    Task 4a complete: Arrow prefab created that flies to the right and damages collision object through SendMessage. Destroyed after lifeTime has ended

---
+++ Assets/Week 5/Scripts/Arrow.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public float speed = 5;
+    public float damage = 1;
+
+    float lifeTime = 5;
+    Vector2 direction;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direction = Vector2.right;
+        rb.SetRotation(-Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg);
+    }
+    private void Update()
+    {
+        if (lifeTime > 0)
+        {
+            lifeTime -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        Destroy(gameObject);
+    }
+}
